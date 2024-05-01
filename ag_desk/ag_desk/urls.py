df72d111a740ed8309ed794d4f0b1100c386a554
miltonso <140776668+miltonso@users.noreply.github.com>
2024-05-01 10:25:11 +1000

    Update urls.py
     path('api/inventory/', include('inventory_management.urls')),

--- ag_desk/ag_desk/urls.py
+++ ag_desk/ag_desk/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('api/', include('farm_management.urls')),  
     path('', include('farm_management.auth_urls')),  # Authentication routes directly under root
     path('employee/', include('employee_management.urls')),  
-    path('inventory/', include('inventory_management.urls')),  
+    path('api/inventory/', include('inventory_management.urls')),  
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
